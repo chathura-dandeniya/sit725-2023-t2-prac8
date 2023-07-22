f4e1ce1595128ead5f0f100ddbf755fbd3656028
chathura-dandeniya <s222489357@deakin.edu.au>
2023-07-22 20:27:48 +1000

    Added ejs to get output
    - I tried to get the output to the HTML page.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ let express = require('express');
 // // Initialize an express application
 let app = express();
 
+app.set('view engine', 'ejs');
+
 // Set the server port. First, it tries to use the PORT environment variable value, 
 // but if it doesn't exist (undefined), it uses 3000 as a default port
 let port = process.env.PORT || 3000;
@@ -26,7 +28,7 @@ app.get('/add', (req, res) => {
     // If 'num1' and 'num2' are valid numbers, send back the sum
     if (!isNaN(number1) && !isNaN(number2)) {
         const sum = number1 + number2;
-        res.json({ sum });
+        res.render('result', { result: sum });
 
         // If 'num1' and/or 'num2' are not valid numbers, 
         // respond with a 400 (Bad Request) status code and an error message
@@ -41,7 +43,7 @@ app.get('/subtract', (req, res) => {
 
     if (!isNaN(number1) && !isNaN(number2)) {
         const subtraction = number1 - number2;
-        res.json({ subtraction });
+        res.render('result', { result: subtraction });
     } else {
         res.status(400).json({ error: 'Invalid numbers provided' });
     }
