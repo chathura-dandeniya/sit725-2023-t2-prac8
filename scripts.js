6dfc3da1f615cecb7b8d54f3f472000d0ba08c6b
chathura-dandeniya <s222489357@deakin.edu.au>
2023-08-05 23:34:50 +1000

    Database Implementation Using MongoDB
    - Send data to the MongoDB server and retrieve data from it.
    - Dynamically implement images by retrieving data from the database.

--- scripts.js
+++ scripts.js
@@ -1,109 +1,57 @@
-const cardList = [{
-    title: 'cat 1',
-    path: 'images/kitten1.jpg',
-    subTitle: 'About Cat 1',
-    description: 'Description of Cat 1'
-},
-{
-    title: 'cat 2',
-    path: 'images/kitten2.jpg',
-    subTitle: 'About Cat 2',
-    description: 'Description of Cat 2'
-},
-{
-    title: 'cat 3',
-    path: 'images/kitten3.jpg',
-    subTitle: 'About Cat 3',
-    description: 'Description of Cat 3'
-}];
-
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">' +
             '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.path + '">' +
             '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.subTitle + '</a></p></div>' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#"></a></p></div>' +
             '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<span class="card-title grey-text text-darken-4">' + item.subTitle + '<i class="material-icons right">close</i></span>' +
             '<p class="card-text">' + item.description + '</p>' +
             '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
 
-const formSumitted = () => {
+const formSubmitted = () => {
     let formData = {};
-    formData.firstName = $('#first_name').val();
-    formData.lastName = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
 
     console.log(formData);
+    postCat(formData);
 }
 
-// $(document).ready(function () {
-//     $('.materialboxed').materialbox();
-//     $('#formSubmit').click(() => {
-//         formSumitted();
-//     });
-//     addCards(cardList);
-//     $('.modal').modal();
-// });
+function postCat(cat) {
+    $.ajax({
+        url: '/api/cat',
+        type: 'POST',
+        data: cat,
+        success: (result) => {
+            if (result.statusCode === 201) {
+                alert('cat posted');
+            }
+        },
+        error: (jqXHR, textStatus, errorThrown) => {
+            alert('An error occurred: ' + textStatus);
+        }
+    });
+}
 
-// //..........Added........................
-// $(document).ready(function () {
-//     $('#formSubmit').click(function (e) {
-//         e.preventDefault();
-
-//         let formData = {
-//             firstName: $('#first_name').val(),
-//             lastName: $('#last_name').val(),
-//             password: $('#password').val(),
-//             email: $('#email').val()
-//         };
-
-//         $.ajax({
-//             type: 'POST',
-//             url: '/submit-form',
-//             data: formData,
-//             success: function () {
-//                 console.log('Data sent successfully');
-//             },
-//             error: function () {
-//                 console.log('There was an error sending the data');
-//             }
-//         });
-//     });
-// });
+function getAllCats() {
+    $.get('/api/cats', (response) => {
+        if (response.statusCode === 200) {
+            addCards(response.data);
+        }
+    });
+}
 
 $(document).ready(function () {
     $('.materialboxed').materialbox();
-    addCards(cardList);
-    $('.modal').modal();
-
-    $('#formSubmit').click(function (e) {
-        e.preventDefault();
-
-        // This function was defined in the first block
-        formSumitted();
-
-        let formData = {
-            firstName: $('#first_name').val(),
-            lastName: $('#last_name').val(),
-            password: $('#password').val(),
-            email: $('#email').val()
-        };
-
-        $.ajax({
-            type: 'POST',
-            url: '/submit-form',
-            data: formData,
-            success: function () {
-                console.log('Data sent successfully');
-            },
-            error: function () {
-                console.log('There was an error sending the data');
-            }
-        });
+    $('#formSubmit').click(() => {
+        formSubmitted();
     });
+    $('.modal').modal();
+    getAllCats();
 });
--- server.js
+++ server.js
@@ -3,47 +3,70 @@ let express = require('express');
 
 // // Initialize an express application
 let app = express();
-
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./myDB.db');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://s222489357:Q3KNsvTCwWFqvO79@cluster0.p9i1kz5.mongodb.net/?retryWrites=true&w=majority";
+let port = process.env.PORT || 3000;
+let collection;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-db.run(`CREATE TABLE IF NOT EXISTS users (
-    firstName TEXT,
-    lastName TEXT,
-    password TEXT,
-    email TEXT
-)`);
-
-app.post('/submit-form', (req, res) => {
-    let data = [req.body.firstName, req.body.lastName, req.body.password, req.body.email];
-    let sql = `INSERT INTO users (firstName, lastName, password, email) VALUES(?,?,?,?)`;
-
-    db.run(sql, data, function (err) {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log(`Row was added to the table with rowid ${this.lastID}`);
-    });
-
-    res.status(200).send('Data has been successfully saved.');
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-// Set the server port. First, it tries to use the PORT environment variable value, 
-// but if it doesn't exist (undefined), it uses 3000 as a default port
-let port = process.env.PORT || 3000;
+async function runDBConnection() {
+    try {
+        await client.connect();
+        collection = client.db().collection('Cats');
+        console.log('Connected to the database.');
+    } catch (ex) {
+        console.error('Error while connecting to the database:', ex);
+    }
+}
 
-// Serve static files (like HTML, CSS, images) from the current directory
 app.use(express.static(__dirname + '/'));
 
-// Handle GET requests to the server root ('/') by rendering 'index.html'
-// (this assumes that a view engine is set up)
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/api/cats', (req, res) => {
+    getAllCats((err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to fetch cats.', error: err.message });
+        }
+        res.json({ statusCode: 200, data: result, message: 'Get All Cats successful' });
+    });
+});
+
+app.post('/api/cat', (req, res) => {
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to post cat.', error: err.message });
+        }
+        res.json({ statusCode: 201, data: result, message: 'success' });
+    });
+});
+
+function getAllCats(callback) {
+    //collection.find({}).toArray(callback);
+    collection.find({}).toArray((err, results) => {
+        console.log(results); // Print the results to check them
+        callback(err, results);
+    });
+}
+
+function postCat(cat, callback) {
+    collection.insertOne(cat, callback);
+}
+
 app.listen(port, () => {
-    console.log('server started');
+    console.log(`Server started on port ${port}`);
+    runDBConnection();
 });
