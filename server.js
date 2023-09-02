23e5e639e6c606315f79325a10e95774c2d13ebe
chathura-dandeniya <s222489357@deakin.edu.au>
2023-09-02 21:07:11 +1000

    Socket implementation

--- server.js
+++ server.js
@@ -4,14 +4,33 @@ let router = require('./routers/router');
 let db = require('./dbConnection');
 let port = process.env.PORT || 3000;
 
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/'));
 app.use('/', router);
 
-const serverInstance = app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('A client has connected');
+    socket.on('disconnect', () => {
+        console.log('A client has disconnected');
+    });
+
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10));
+    }, 1000);
+})
+
+const serverInstance = http.listen(port, () => {
     console.log(`Server started on port ${port}`);
     db.connect();
 });
+
+// const serverInstance = app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+//     db.connect();
+// });
 
 module.exports = serverInstance; // Export server for testing (newly added)
